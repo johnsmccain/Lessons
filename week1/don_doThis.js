b449c8b161d666b3ebe6d8cdb59567bc176b4e86
johnsmccain <johnsdanlami@gmail.com>
2025-08-04 13:11:27 +0100

    initial push

---
+++ week1/don_doThis.js
@@ -0,0 +1,9 @@
+
+let percentage = 30*50 ;
+console.log(percentage)
+
+percentage = 50*30 ;
+console.log(percentage);
+
+percentage = 60*40;
+console.log(percentage )
---
+++ week1/myVariables.js
@@ -0,0 +1,69 @@
+
+// console.log("Hello Boma!");
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+// Variables
+// initialize
+// let x = 5;
+// console.log(x)
+
+// // change the value of x
+// x = 9;
+// console.log(x)
+
+// x = "Boma";
+// console.log(x)
+
+// x = true;
+// console.log(x)
+// These are valid variable names
+let x = 5;
+
+// // Declaration
+// function addTwoNumbers (num1, num2){
+//     // num1 = 1
+//     // num2 = 2
+//     console.log(num1 + num2 );
+// }
+
+// // Execution
+// addTwoNumbers(1, 2);
+
+// function calculateInPecentage(capital, dividen) {
+//     let percentage = capital*dividen;
+   
+// }
+
+// let first = calculateInPecentage(30, 50)
+// calculateInPecentage(50, 30)
+// calculateInPecentage(60, 40)
+// console.log(calculateInPecentage(50, 30))
+
+
+// console.log()
+// console.log(first)
+
+// function myFunction(p1, p2) {
+//   return p1 * p2;
+// }
+
+// console.log(myFunction(3,5))
+const Person = {
+    name: "Boma",
+    age: 25,
+    isAGirl: true,
+    hobbies: ["Dancing", "Reading"]
+}
+console.log(Person["isAGirl"])
+
+// Person.age = 27
+// console.log(Person)
+
+const arr = ['Boma', "John", "Jane", {age: 57}]
+// console.log(arr)
+// arr[0] = "Dan"
+// console.log(arr)
+
+// console.log(arr[3])
