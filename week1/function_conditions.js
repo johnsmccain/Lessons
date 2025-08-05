df66855d55fc98455615edc052984fc1471e5341
johnsmccain <johnsdanlami@gmail.com>
2025-08-05 11:47:50 +0100

    functions types

---
+++ week1/function_conditions.js
@@ -0,0 +1,30 @@
+// Types of functions
+
+// 1. Function with no parameters and no return value
+// function firstFunc(){
+//     console.log("Hello First from line 5!");
+// }
+// console.log(firstFunc())
+// 2. Function with parameters and no return value
+// function secondFuncWithParams(name){
+//     console.log(name)
+// }
+// secondFuncWithParams("Boma");
+
+// 3. Function with no parameters and return value
+// function thirdFuncWithReturn() {
+//     return "Boma from line 10"
+// }
+// console.log(thirdFuncWithReturn())
+// 4. Function with parameters and return value
+// function sayHello(name) {
+//     return "Hello " + name 
+// }
+// console.log(sayHello("Boma"))
+
+
+
+// Arrow function
+// 1 Function with no parameters and no return value
+// const func1 = () => console.log("Hello from line 29")
+// console.log(func1())
