86a953e565d47b82685a72a3e9566cc850c6bdbd
johnsmccain <johnsdanlami@gmail.com>
2025-08-08 13:56:53 +0100

    Half way

---
+++ week1/studentManagement.js
@@ -0,0 +1,48 @@
+/**
+ * Problem Statement
+Write a program that:
+1. Accepts a list of student names and their scores.
+2. Uses a function to calculate a grade based on the score:
+90–100: A
+80–89: B
+70–79: C
+60–69: D
+<60: F
+3. Stores the students and their grades in an array.
+4. Displays each student’s name, score, and grade using a loop.
+5. Uses conditions (if, else if) inside the grading function.
+6. Bonus: Add logic to find and print the highest score and the average score.
+Requirements
+Use arrays to store student data.
+Use functions for grade calculation and average score.
+Use variables for storing input/output data.
+Use loops to iterate over student records.
+Use conditional statements to determine letter grades.
+ */
+
+const studentList = [
+    {name:"Boma", score: 75},
+    {name: "John", score: 76},
+    {name: "Nora", score: 77}
+]
+
+function calculateGrade(score){
+    if(score => 90 && score <=100){
+        return "A"
+    } else if(score => 80 && score <= 89){
+        return "B"
+    }
+}
+console.log(calculateGrade(80))
+
+
+
+function aggregateStudentScores(students){
+    for(let i = 0; i < studentList.length; i++){
+        let name = students[i].name;
+        let score = students[i].score;
+        console.log(name, score)
+    }
+}
+
+// aggregateStudentScores(studentList)
