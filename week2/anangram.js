fa217c4c80ad3f0bc2625370050e9ec680c89015
johnsmccain <johnsdanlami@gmail.com>
2025-08-19 13:45:39 +0100

    hashmap

---
+++ week2/anangram.js
@@ -0,0 +1,23 @@
+let input = "pit";
+let target = "tip";
+function anagram(x, y){
+    if (x.length !== y.length) {
+        return "not an anagram";
+    }
+
+    const hashMap = {}
+    for(let i = 0; i < x.length; i++){
+    
+    }
+
+}
+
+const state = anagram(input, target)
+console.log(state)
+
+
+
+
+
+
+
--- week2/simple.js
+++ week2/simple.js
@@ -4,12 +4,18 @@
 //     console.log(i)
 // }
 
-let a = []
-a.push(6)
-a.push(8)
-a.push("Boma")
-a.push([1,2,3])
-a.push({name: "John", age: 26})
-console.log("Array", a)
-console.log("Length", a.length)
-console.log(a[4])
+// let a = []
+// a.push(6)
+// a.push(8)
+// a.push("Boma")
+// a.push([1,2,3])
+// a.push({name: "John", age: 26})
+// console.log("Array", a)
+// console.log("Length", a.length)
+// console.log(a[4])
+
+const Laptop = {}
+Laptop["color"] = "ash"
+let property = Laptop.hasOwnProperty("colot")
+console.log(property)
+console.log(Laptop)
